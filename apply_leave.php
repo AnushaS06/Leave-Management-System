55b1d1a5d8ed9a7c191968935ce7a3c2d002de77
Anusha S <40312715+AnushaS06@users.noreply.github.com>
2018-12-05 12:55:26 +0530

    Update apply_leave.php

--- apply_leave.php
+++ apply_leave.php
@@ -1,21 +1,5 @@
 <?php
- $mysqli = new mysqli("localhost", "root", "anusha@123", "leave_management");
-// $id = strval($_GET['id']);
-// $no_of_days=strval($_GET['no']);
-// $from_date=strval($_GET['fdate']);
-// $to_date=strval($_GET['tdate']);
-// $updated_date=strval($_GET['udate']);
-// $type_id=strval($_GET['type']);
-// $status=strval($_GET['status']);
-// //echo("INSERT into employee_leave_applied (type_id,emp_id,from_date,to_date,no_of_days,status,updated_date)
-// //VALUES (".$type_id." ,'".$id."', '".$from_date."', '".$to_date."', ".$no_of_days.", '".$status."', '".$updated_date."')");
-// $result = $mysqli->query("INSERT into employee_leave_applied ela (ela.type_id,ela.emp_id,ela.from_date,ela.to_date,ela.no_of_days,ela.status,ela.updated_date) VALUES (".$type_id." ,'".$id."', '".$from_date."', '".$to_date."', ".$no_of_days.", '".$status."', '".$updated_date."')");
-// var_dump($result);
-
-// if ($result)
-//     http_response_code(200);
-// else 
-//     http_response_code(400);
+ $mysqli = new mysqli("localhost", "username", "password", "database_name");
 $id = strval($_GET['id']);
 $no_of_days=strval($_GET['no']);
 $from_date=strval($_GET['fdate']);
