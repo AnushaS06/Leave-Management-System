38cd2c38fb0910be84dbf4171bf60a2f1eff8e72
Anusha S <40312715+AnushaS06@users.noreply.github.com>
2018-12-05 12:56:21 +0530

    Update left_leave.php

--- left_leave.php
+++ left_leave.php
@@ -1,12 +1,8 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "anusha@123", "leave_management");
+$mysqli = new mysqli("localhost", "username", "password", "database_name");
 $id = strval($_GET['id']);
-//echo($id);
-//$p = strval($_GET['p']);
-//echo($p);
 $result = $mysqli->query("SELECT e.type_id,e.days_available FROM employee_leave_availability e where e.emp_id='" . $id . "'");
 $resp=[];
-//var_dump($result,$id);
 if ($result->num_rows > 0) {
     foreach ($result as $r)
     {    
